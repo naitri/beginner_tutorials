31fd487a05ac270d1f030f7f26a16b47c07a11b8
Naitri <naitrir21@gmail.com>
2021-11-15 12:11:45 -0500

    Added tf broadcaster in talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,65 +1,72 @@
 /******************************************************************************
- * MIT License
- * 
- * Copyright (c) 2021 Naitri Rajyaguru
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
- ******************************************************************************/
+BSD 3-Clause License
 
+Copyright (c) 2021, Mayank Joshi
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
 
 /**
  * @file talker.cpp
- * @author Naitri Rajyaguru
- * @brief publishing simple custom string using service
+ * @author mayankJoshi (mayankjoshi63@gmail.com)
+ * @brief Initializes a "talker_node" which publishes on topic "/chatter"
  * @version 0.1
- * @date 2021-11-08
+ * @date 2021-11-06
  * 
  * @copyright Copyright (c) 2021
  * 
  */
 
-
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
 #include <std_msgs/String.h>
+#include <beginner_tutorials/update.h>
+
+#include <sstream>
 
 
 /**
- * Import header file of service
+ * Default String published by talker
  */
-#include <beginner_tutorials/update.h>
-#include <sstream>
-
 extern std::string message = "Software Development for Robotics";
+
+/**
+ * Default loop frequency, Hz
+ */
 int default_rate = 10;
 
 /**
- * This tutorial demonstrates simple sending custom messages over the ROS system.
- */
-
-/**
  * @brief Function to call service
- * @param request is input string to service
- * @param response is updated string by service
+ * @param request Input string to service
+ * @param response Response sent by service 
  * @return bool
  */
-bool msg_modifier(beginner_tutorials::update::Request *request,
-  beginner_tutorials::update::Response *response) {
+bool msg_modifier(beginner_tutorials::update::Request &request,
+  beginner_tutorials::update::Response &response) {
   ROS_INFO_STREAM("Message modification service is called..");
   if (request.input.empty()) {
     ROS_ERROR_STREAM("Invalid empty message..modificationis denied");
@@ -72,6 +79,8 @@ bool msg_modifier(beginner_tutorials::update::Request *request,
     return true;
   }
 }
+
+  
 int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
@@ -83,51 +92,28 @@ int main(int argc, char **argv) {
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-
   ros::init(argc, argv, "talker");
 
+  // Initializing tf
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+  transform.setOrigin( tf::Vector3(0.0, 1.0, 0.0) );
+  tf::Quaternion q;
+  q.setRPY(0, 0, 1);
+  transform.setRotation(q);
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-
-  ros::NodeHandle n;
-
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-
-  auto serv = n.advertiseService("update", &msg_modifier);
-
+  // variable to store the loop frequency, set to default_freq
   int rate = 0;
-  if (argc > 0) {
-    ROS_DEBUG_STREAM("The change in rate will be:" << rate);
-    rate = atoi(argv[1]);
-  }
 
-  if (rate == 0) {
+  if ( argc > 1 ) {
+    // set frequency according to parsed argument
+    rate = atoi(argv[1]);
+  } else if ( rate == 0 ) {
     ROS_ERROR_STREAM("Rate cannot be zero");
     ROS_WARN_STREAM("Default rate will be set..");
     rate = default_rate;
-  } else if (rate < 0) {
+
+  } else if ( rate < 0 ) {
     ROS_FATAL_STREAM("Rate cannot be non-negative");
     ROS_WARN_STREAM("Default rate will be set..");
 
@@ -135,26 +121,55 @@ int main(int argc, char **argv) {
     ROS_INFO_STREAM("Rate is now changed to " << rate);
     rate = rate; }
 
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /**
+ * The advertise() function is how you tell ROS that you want to
+ * publish on a given topic name. This invokes a call to the ROS
+ * master node, which keeps a registry of who is publishing and who
+ * is subscribing. After this advertise() call is made, the master
+ * node will notify anyone who is trying to subscribe to this topic name,
+ * and they will in turn negotiate a peer-to-peer connection with this
+ * node.  advertise() returns a Publisher object which allows you to
+ * publish messages on that topic through a call to publish().  Once
+ * all copies of the returned Publisher object are destroyed, the topic
+ * will be automatically unadvertised.
+ *
+ * The second parameter to advertise() is the size of the message queue
+ * used for publishing messages.  If messages are published more quickly
+ * than we can send them, the number here specifies how many messages to
+ * buffer up before throwing some away.
+ */
+
+  ros::Publisher chatter_pub =
+  n.advertise<std_msgs::String>("/chatter", 10);
+
+
+  auto server = n.advertiseService("update", msg_modifier);
 
   ros::Rate loop_rate(rate);
-
 
   /**
    * A count of how many messages we have sent. This is used to create
    * a unique string for each message.
    */
-
   int count = 0;
   while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
     std_msgs::String msg;
-    std::stringstream ss;
-    ss << message << count;
-    msg.data = ss.str();
-    ROS_INFO_STREAM(msg.data.c_str());
 
+    std::stringstream ss;
+    ss << message << " " << count;
+    msg.data = ss.str();
+
+    ROS_INFO_STREAM(msg.data.c_str());
 
     /**
      * The publish() function is how you send messages. The parameter
@@ -162,20 +177,14 @@ int main(int argc, char **argv) {
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-
     chatter_pub.publish(msg);
-
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "talk"));
 
     ros::spinOnce();
 
-
-
     loop_rate.sleep();
-
     ++count;
   }
 
-
   return 0;
 }
-
